6be1c9cc4033649fe33a853b3d6443954bf1c02d
Brian Yee <brian.a.yee@gmail.com>
2011-03-26 19:47:53 -0400

    added runaway links

--- eyeexam.js
+++ eyeexam.js
@@ -13,19 +13,30 @@
 
     $.fn.eyeexam = function(options) {
 
+
+// ==============
+// ! SETUP AND DEFAULTS  
+// ==============
         //Defaults to extend options
 		var defaults = {
 			delay: 800,
-			textElements: ['p', 'h1', 'h2', 'h3', 'h4']
+			blur: 2,
+			textElements: ['p', 'a', 'strong', 'em', 'h1', 'h2', 'h3', 'h4'],
+
+			offset: 10
 		}; 
         
         //Extend those options
         var options = $.extend(defaults, options); 
 	
         return this.each(function() {
-			/* Utility Functions */
+// ==============
+// ! UTILITY FUNCTIONS   
+// ==============
 
-			/* rgbToHex, converts RGB to hex as a string with a '#' */
+			/***** rgbToHex*****/
+			/* string */
+			/* Converts RGB to hex as a string with a '#' */
 			function rgbToHex(rgb) {
 				if (rgb.match(/^#[0-9A-Fa-f]{6}$/)) { return rgb; }
 				var rgbvals = /rgb\((.+),(.+),(.+)\)/i.exec(rgb);
@@ -41,10 +52,11 @@
 				return '#' + pad(rval.toString(16)) + pad(gval.toString(16)) + pad(bval.toString(16));
 			} 
 
+// ==============
+// ! EVIL CODE GOES HERE   
+// ==============
 
-			/* Include evil code here */
-
-			/* Text Shadow */
+			/***** Text Shadow *****/
 
 			options.textElements.map(function(value) {
 				var textElem = $(value);
@@ -52,10 +64,45 @@
 					var textColor = rgbToHex(textElem.css('color'));
 
 					textElem.css('textShadow',
-						textColor + ' 0 2px 2px, '
-						+ textColor + ' 0 -2px 2px'
+						textColor + ' 0 0 ' + options.blur + 'px, '
+						+ textColor + ' 0 0 ' + options.blur + 'px'
 					);
 				}
+			});
+
+			/***** Runaway Links *****/
+
+			$('a').hover( function(event) {
+				var elem = $(event.target);
+				
+				elem.css({
+					position: 'relative'
+				});
+
+				var offsetAmt = {left: '+=', top: '+='};
+				switch(Math.floor(Math.random()*4)) {
+					case 0:
+						offsetAmt.left += ''+options.offset;
+						offsetAmt.top += ''+options.offset;
+					break;
+					case 1:					
+						offsetAmt.left += '-'+options.offset;
+						offsetAmt.top += ''+options.offset;
+					break;
+					case 2:
+						offsetAmt.left += '-'+options.offset;
+						offsetAmt.top += '-'+options.offset;
+					break;
+					case 3:
+						offsetAmt.left += ''+options.offset;
+						offsetAmt.top += '-'+options.offset;
+					break;
+				}
+				elem.stop().animate({
+					duration: 500,
+					left: offsetAmt.left,
+					top: offsetAmt.top,
+  				});
 			});
 
 
